680a773ecab331dfca6934a7bcdd5123db23a32e
magnusjovik <magnusjovik@hotmail.com>
2021-11-10 15:15:10 +0100

    carousel proof of concept

---
+++ subpages/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <title>Magnus Jovik</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="../slide.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+
+      <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid ">
+        <a class="navbar-brand" href="../../index.html">Magnus Jovik</a>
+        <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto">
+            <a class="nav-link" href="../photo.html">photography</a>
+            <a class="nav-link" href="../film.html">film</a>
+            <a class="nav-link" href="https://www.instagram.com/magnus_jovik/" target="_blank">instagram</a>
+            <a class="nav-link" href="../contact.html">contact</a>
+            <a class="nav-link" href="../about.html">about</a>
+
+          </div>
+        </div>
+        </div>
+      </nav>
